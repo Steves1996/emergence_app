f0177216ec20022881cc8af4e22053a3b084efa5
FOPABLONDEL <amadjoblondel@gmail.com>
2023-08-25 05:48:16 +0100

    print modification
    Signed-off-by: FOPABLONDEL <amadjoblondel@gmail.com>

--- medicine_purchase_print_pdf.php
+++ medicine_purchase_print_pdf.php
@@ -44,19 +44,23 @@ function generateRow()
 }
 
 require_once('tcpdf/tcpdf.php');
-$pdf = new TCPDF('P', PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 $pdf->SetCreator(PDF_CREATOR);
 $pdf->SetTitle("CM-Emergence");
-$pdf->SetHeaderData('', '', PDF_HEADER_TITLE, PDF_HEADER_STRING);
+
+$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE, PDF_HEADER_STRING, array(0, 64, 255), array(0, 64, 128));
+$pdf->setFooterData(array(0, 64, 0), array(0, 64, 128));
+// set document information
+//$pdf->SetHeaderData('assets/emergence_logo.png', '100', PDF_HEADER_TITLE, PDF_HEADER_STRING);
 $pdf->setHeaderFont(array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
 $pdf->setFooterFont(array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
 $pdf->SetDefaultMonospacedFont('helvetica');
 $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
 $pdf->SetMargins(PDF_MARGIN_LEFT, '10', PDF_MARGIN_RIGHT);
-$pdf->setPrintHeader(false);
-$pdf->setPrintFooter(false);
+$pdf->setPrintHeader(true);
+$pdf->setPrintFooter(true);
 $pdf->SetAutoPageBreak(TRUE, 10);
-$pdf->SetFont('helvetica', '', 11);
+$pdf->SetFont('helvetica', '', 12);
 $pdf->AddPage();
 $content = '';
 $content .= '
--- repport_print_pdf.php
+++ repport_print_pdf.php
@@ -8,16 +8,11 @@ function generateRow()
 
     $object = new db();
 
-    
+
     $from_date = $_SESSION['from_date'];
     $to_date = $_SESSION['to_date'];
 
-    print_r('date session 1 : '); 
-    print_r( $from_date); 
-    print_r('date session 2 : '); 
-    print_r($to_date);
-
-    if(isset($from_date) && isset($to_date)){
+    if (isset($from_date) && isset($to_date)) {
         $where_date = "WHERE om.order_added_on BETWEEN '" . $from_date . "' AND '" . $to_date . "' ";
 
         $object->query = "
@@ -31,13 +26,10 @@ function generateRow()
         ON om.order_created_by = um.user_id
         $where_date
         ";
-    
+
         //use for MySQLi OOP
         $result = $object->get_result();
-        
-        print_r('result 1'); 
-        print_r($result);
-    }else{
+    } else {
         $object->query = "
         SELECT mm.medicine_name, oim.medicine_quantity, oim.medicine_price, om.order_added_on, um.user_name
         FROM `order_item_msbs` oim 
@@ -49,13 +41,12 @@ function generateRow()
         ON om.order_created_by = um.user_id
         ";
         $result = $object->get_result();
-        print_r('result 2'); 
+        print_r('result 2');
         print_r($result);
-
     }
 
     //use for MySQLi OOP
-   
+
     $count_medicine = 0;
     $TotalAmount = 0;
 
@@ -82,39 +73,43 @@ function generateRow()
 }
 
 require_once('tcpdf/tcpdf.php');
-$pdf = new TCPDF('P', PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 $pdf->SetCreator(PDF_CREATOR);
 $pdf->SetTitle("CM-Emergence");
-$pdf->SetHeaderData('', '', PDF_HEADER_TITLE, PDF_HEADER_STRING);
+
+$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE, PDF_HEADER_STRING);
+$pdf->setFooterData(array(0, 64, 0), array(0, 64, 128));
+// set document information
+//$pdf->SetHeaderData('assets/emergence_logo.png', '100', PDF_HEADER_TITLE, PDF_HEADER_STRING);
 $pdf->setHeaderFont(array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
 $pdf->setFooterFont(array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
 $pdf->SetDefaultMonospacedFont('helvetica');
 $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
 $pdf->SetMargins(PDF_MARGIN_LEFT, '10', PDF_MARGIN_RIGHT);
-$pdf->setPrintHeader(false);
-$pdf->setPrintFooter(false);
+$pdf->setPrintHeader(true);
+$pdf->setPrintFooter(true);
 $pdf->SetAutoPageBreak(TRUE, 10);
-$pdf->SetFont('helvetica', '', 11);
+$pdf->SetFont('helvetica', '', 12);
 $pdf->AddPage();
 $content = '';
 $content .= '
-      	<h2 align="center">CM-Emergence</h2>
+    <div>
       	<h4>Rapport des Ventes</h4>
-      	<table border="1" cellspacing="0" cellpadding="1">
+      	<table border="1" cellspacing="0" cellpadding="2">
            <tr>
 				<th width="5%"><b>Sr.</b></th>
-				<th><b>Nom du medicament</b></th>
-				<th><b>date de vente</b></th>
+				<th width="30%"><b>Nom du medicament</b></th>
+				<th width="25%"><b>date de vente</b></th>
 				<th><b>Vendeur</b></th>
-				<th><b>Quantite vendu</b></th>
-				<th><b>Prix total</b></th>
+				<th width="8%"><b>Qte vendu</b></th>
+				<th width="15%"><b>Prix total</b></th>
            </tr>
 ';
 
 $content .= generateRow();
 $date  = date('Y-m-d');
 $pdf_name = 'repport_for_' . $date . '.pdf';
-$content .= '</table>';
+$content .= '</table></div>';
 $pdf->writeHTML($content);
 ob_end_clean();
 $pdf->Output($pdf_name, 'I');
